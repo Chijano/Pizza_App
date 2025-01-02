46c568c1b406f15e44181a8d639f7490defa9bd9
Martin Matuštík <martin.matustik@outlook.cz>
2025-01-02 13:38:29 +0100

    Added pizzas

--- Models/Pizza.py
+++ Models/Pizza.py
@@ -12,14 +12,7 @@ class Pizza:
         else:
             raise ValueError("Only instances of Topping can be added.")
 
-    def calculate_price(self):
-        return sum(topping.price for topping in self.toppings)
 
-    def __str__(self):
-        toppings_list = ", ".join(str(topping.name) for topping in self.toppings)
-        return f"Pizza '{self.name}' with toppings: {toppings_list} - Total: {self.calculate_price()} CZK"
-
-    # Predefined pizzas as a class-level dictionary
     basic_pizzas = {
         "Margherita": ["Cheese"],
         "Hawaiian": ["Cheese", "Ham", "Pineapple"],
