a5ff6e9643b9c37b0e636ef750c3c402d03dd590
Martin Matuštík <martin.matustik@outlook.cz>
2025-01-03 15:46:38 +0100

    Pizzas added

--- Models/Pizza.py
+++ Models/Pizza.py
@@ -2,38 +2,106 @@ from Topping import Topping
 
 
 class Pizza:
-    def __init__(self, name, toppings=None):
-        self.name = name
-        self.toppings = toppings if toppings else []
+    def __init__(self):
+        self.description = "Basic Pizza"
+        self.price = 100
 
-    def add_topping(self, topping):
-        if isinstance(topping, Topping):
-            self.toppings.append(topping)
-        else:
-            raise ValueError("Only instances of Topping can be added.")
+    def get_description(self):
+        return self.description
+
+    def get_price(self):
+        return self.price
 
 
-    basic_pizzas = {
-        "Margherita": ["Cheese"],
-        "Hawaiian": ["Cheese", "Ham", "Pineapple"],
-        "Pepperoni": ["Cheese", "Salami"],
-        "Veggie": ["Cheese", "Bell Pepper", "Mushrooms", "Onion", "Olives"],
-        "BBQ Chicken": ["Cheese", "Chicken", "Bacon", "Onion", "BBQ Sauce"],
-        "Meat Lover's": ["Cheese", "Ham", "Salami", "Bacon"],
-        "Four Cheese": ["Cheese", "Mozzarella", "Parmesan", "Blue Cheese"],
-        "Seafood": ["Cheese", "Shrimp", "Tuna", "Garlic"],
-        "Spicy": ["Cheese", "Salami", "Jalapeños", "Onion"],
-        "Supreme": ["Cheese", "Ham", "Salami", "Bell Pepper", "Mushrooms", "Olives"],
-    }
+def add_topping(topping_name):
+    def decorator(pizza_func):
+        def wrapper():
+            pizza1 = pizza_func()
+            topping_price = Topping.toppings.get(topping_name, 0)
+            pizza1.description += f", {topping_name}"
+            pizza1.price += topping_price
+            return pizza1
+        return wrapper
+    return decorator
 
-    def calculate_price(self):
-        """Calculate the total price of the pizza."""
-        return sum(topping.price for topping in self.toppings)
 
-    @classmethod
-    def create_basic_pizzas(cls):
-        pizzas = []
-        for name, topping_names in cls.basic_pizzas.items():
-            toppings = [Topping(topping, Topping.toppings[topping]) for topping in topping_names]
-            pizzas.append(cls(name, toppings))
-        return pizzas
+@add_topping("Cheese")
+@add_topping("Ham")
+def classic_pizza():
+    return Pizza()
+
+
+@add_topping("Salami")
+@add_topping("Mushrooms")
+def salami_mushroom_pizza():
+    return Pizza()
+
+
+@add_topping("Olives")
+@add_topping("Bell Pepper")
+@add_topping("Onion")
+def vegetarian_pizza():
+    return Pizza()
+
+
+@add_topping("BBQ Sauce")
+@add_topping("Chicken")
+@add_topping("Cheese")
+def bbq_chicken_pizza():
+    return Pizza()
+
+
+@add_topping("Shrimp")
+@add_topping("Mozzarella")
+@add_topping("Garlic")
+def seafood_pizza():
+    return Pizza()
+
+
+@add_topping("Bacon")
+@add_topping("Cheese")
+@add_topping("Onion")
+def bacon_onion_pizza():
+    return Pizza()
+
+
+@add_topping("Blue Cheese")
+@add_topping("Parmesan")
+@add_topping("Cheese")
+@add_topping("Mozzarella")
+def four_cheese_pizza():
+    return Pizza()
+
+
+@add_topping("Tuna")
+@add_topping("Onion")
+def tuna_onion_pizza():
+    return Pizza()
+
+
+@add_topping("Jalapeños")
+@add_topping("Salami")
+@add_topping("Mozzarella")
+def spicy_salami_pizza():
+    return Pizza()
+
+
+@add_topping("Pineapple")
+@add_topping("Cheese")
+@add_topping("Ham")
+def hawaiian_pizza():
+    return Pizza()
+
+
+recipes = [
+    ("Hawaiian Pizza", classic_pizza),
+    ("Salami and Mushroom Pizza", salami_mushroom_pizza),
+    ("Vegetarian Pizza", vegetarian_pizza),
+    ("BBQ Chicken Pizza", bbq_chicken_pizza),
+    ("Seafood Pizza", seafood_pizza),
+    ("Bacon and Onion Pizza", bacon_onion_pizza),
+    ("Four Cheese Pizza", four_cheese_pizza),
+    ("Tuna and Onion Pizza", tuna_onion_pizza),
+    ("Spicy Salami Pizza", spicy_salami_pizza),
+    ("Sweet and Savory Pizza", hawaiian_pizza),
+]
